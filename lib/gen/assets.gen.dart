cca4284ac5440570c5d57bd5f92f5f75842c4517
Aslı Bayram <asli.bayram.tr@gmal.com>
2025-07-16 00:26:40 +0300

    assets hatalari duzeltildi

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,44 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/Poppins-Bold.ttf
+  String get poppinsBold => 'assets/fonts/Poppins-Bold.ttf';
+
+  /// File path: assets/fonts/Poppins-Regular.ttf
+  String get poppinsRegular => 'assets/fonts/Poppins-Regular.ttf';
+
+  /// List of all assets
+  List<String> get values => [poppinsBold, poppinsRegular];
+}
+
+class $AssetsSoundsGen {
+  const $AssetsSoundsGen();
+
+  /// File path: assets/sounds/card_flip.mp3
+  String get cardFlip => 'assets/sounds/card_flip.mp3';
+
+  /// File path: assets/sounds/level_complete.mp3
+  String get levelComplete => 'assets/sounds/level_complete.mp3';
+
+  /// File path: assets/sounds/match_success.mp3
+  String get matchSuccess => 'assets/sounds/match_success.mp3';
+
+  /// List of all assets
+  List<String> get values => [cardFlip, levelComplete, matchSuccess];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsSoundsGen sounds = $AssetsSoundsGen();
+}
